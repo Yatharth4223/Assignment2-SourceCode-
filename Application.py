2a774741b983f73de7c501dc31732b0bc97436a2
Yatharth <Yatharth.jain4223@gmail.com>
2022-10-12 00:15:13 -0400

    created vraibales and attributes for user class

---
+++ Application.py
@@ -0,0 +1,40 @@
+'''This Program is created by Yatharth Jain'''
+
+#import User from user
+import User from user
+
+#create class Application
+
+    # HelloMessage function
+
+    #TakeUserInput function
+        #Enter product code
+
+        # enter product name
+
+        # enter current stock
+
+        # enter product sale price
+
+        # enter product manufacture cost
+
+        # enter monthly estimated production cost
+
+    # displayUserInput()
+        
+        #print product code 
+
+        #print product name
+
+        #print sale price
+
+        # print manufacture cost
+
+        # print monthly production
+
+        #create a for loop for months
+            #create an instance of User class, i.e customer 
+            #month i:
+                #manufactured
+                #return customer.itemsSold
+                #return customer.stocked
